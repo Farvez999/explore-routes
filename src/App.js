58f7200d0e45e9e447a31f5d23b5c613f476f0ce
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-10-03 20:20:45 +0600

    Create simple route with Link and components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Product from './components/Product/Product';
+import About from './components/About/About';
 
 function App() {
+  const router = createBrowserRouter([
+    { path: '/', element: <div>This is Default Page</div> },
+    { path: '/home', element: <Home></Home> },
+    { path: '/product', element: <Product></Product> },
+    { path: '/about', element: <About></About> }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/components/Product/Product.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Product = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h3>This is Product Page</h3>
+        </div>
+    );
+};
+
+export default Product;
